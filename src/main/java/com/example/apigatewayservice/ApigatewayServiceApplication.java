0add533db72adedc59b77c790f3485c39f090898
oksk1111 <oksk1111@gmail.com>
2022-07-25 22:52:00 +0900

    암호화

--- src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,13 +1,22 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayServiceApplication.class, args);
+	}
+
+	// Actuator 에서 httptrace 옵션이 있다면 빈 등록해주어야 한다.
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
